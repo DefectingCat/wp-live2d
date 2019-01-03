061a6a77690ca12b8fa32ef9ccb31a5924c9205c
DefectingCat <defect.y@outlook.com>
2019-01-03 09:43:35 +0800

    update

---
+++ umaru/js/load.js
@@ -0,0 +1,4 @@
+    loadlive2d("paul", "https://cdn.defectink.com/live2d/sagiri/model.json");
+	if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    document.getElementById("paul").width="0";
+} else {
